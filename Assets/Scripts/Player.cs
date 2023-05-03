2615789c8980b84f63cf229165992ef4f9c560fa
Connor Betts <112406724+Connor-Betts-student@users.noreply.github.com>
2023-05-03 10:39:12 -0400

    stuff

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+[SerializeField] float walkspeed = 5.0f;
+
+
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
